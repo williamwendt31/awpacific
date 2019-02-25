4005ec3824ff515fa22cca28167ed95e64703e6f
williamwendt31 <williamwendt31@yahoo.com>
2019-02-25 13:50:15 -0800

    mdbootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import "./index.css";
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MDBContainer>
+        <MDBRow center style={{ height: "100vh" }}>
+          <MDBCol middle="true" sm="8" className="text-center">
+            <img src={logo} alt="logo" style={{ width: "10rem" }} />
+            <h1>Welcome to Your MDB React App</h1>
+            <p className="mb-2">The application is configured and ready to import our components.</p>
+            <MDBBtn href="https://mdbootstrap.com/docs/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></MDBBtn>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     );
   }
 }
--- server/config/routes/products.js
+++ server/config/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const products = require('../../controllers/products');
 
 router.get('/', (req, res) => {
-
+    
 });
 
 module.exports = router;
